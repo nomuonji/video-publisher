4a528b68951e5a80e770221c35d0a540e90caddf
nomuonji <youphone0613@icloud.com>
2025-10-11 02:24:09 +0900

    test

--- api/post/index.ts
+++ api/post/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { performVideoPosting } from '../_lib/postVideo';
+import { performVideoPosting } from '../_lib/postVideo.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
